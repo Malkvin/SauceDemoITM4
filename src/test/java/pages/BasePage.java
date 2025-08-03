4fbf2f102a3294fd4c392f5cc7c2adc29fe0038b
Malkvin <119970233+Malkvin@users.noreply.github.com>
2025-08-04 02:59:11 +0300

    add TestNG

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,13 +1,18 @@
 package pages;
 
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 
 public class BasePage {
 
     protected final String BASE_URL = "https://www.saucedemo.com/";
     WebDriver driver;
+    JavascriptException js = (JavascriptException) driver;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
     }
-}
+    }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,12 @@
 package tests;
 
+import org.openqa.selenium.JavascriptException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 import pages.CartPage;
 import pages.LoginPage;
@@ -12,7 +14,7 @@ import pages.ProductsPage;
 
 import java.time.Duration;
 import java.util.HashMap;
-
+@Listeners(TestListener.class)
 public class BaseTest {
 
     WebDriver driver;
@@ -21,21 +23,26 @@ public class BaseTest {
     ProductsPage productsPage;
     CartPage cartPage;
 
+    @Parameters({"browser"})
     @BeforeMethod
-    public void setup() {
-        softAssert = new SoftAssert();
-        ChromeOptions options = new ChromeOptions();
-        HashMap<String, Object> chromePrefs = new HashMap<>();
-        chromePrefs.put("credentials_enable_service", false);
-        chromePrefs.put("profile.password_manager_enabled", false);
-        options.setExperimentalOption("prefs", chromePrefs);
-        options.addArguments("--incognito");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--disable-infobars");
+    public void setup(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            HashMap<String, Object> chromePrefs = new HashMap<>();
+            chromePrefs.put("credentials_enable_service", false);
+            chromePrefs.put("profile.password_manager_enabled", false);
+            options.setExperimentalOption("prefs", chromePrefs);
+            options.addArguments("--incognito");
+            options.addArguments("--disable-notifications");
+            options.addArguments("--disable-popup-blocking");
+            options.addArguments("--disable-infobars");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }
 
-        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        softAssert = new SoftAssert();
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
